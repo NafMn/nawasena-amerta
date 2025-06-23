c9a75c0f6c347691c0920080538049f64544d1ba
NafMn <blptnafis@gmail.com>
2025-06-23 23:27:46 +0700

    Add Tailwind CSS configuration file with font family settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', "sans-serif"],
+        jakarta: ['"Plus Jakarta Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
